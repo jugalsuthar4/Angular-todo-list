e9dd6c5c9f1ed832e7e133526c6a5b4b79cc03b8
jugalsuthar <jugalsutharpro@gmail.com>
2022-04-22 20:37:34 +0530

    todo list made

---
+++ src/app/mycomponent/todos/todos.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../Todo"
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  localItem:string;
+  constructor() {
+   this.localItem=localStorage.getItem('todos');
+   if(this.localItem==null){
+     this.todos=[];
+   }
+   else{
+     this.todos=JSON.parse(this.localItem);
+   }
+
+  }
+
+  ngOnInit(): void {
+  }
+  deleteTodo(todo: Todo) {
+   // console.log(todo);
+    const index = this.todos.indexOf(todo);
+    if (index > -1) {
+      this.todos.splice(index, 1); // 2nd parameter means remove one item only
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    }
+  }
+  addTodo(todo:Todo){
+    this.todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+  toggleTodo(todo:Todo){
+    const index = this.todos.indexOf(todo);
+    console.log(todo)
+    this.todos[index].active=!this.todos[index].active;
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+
+}
